c495079fa35ad1313c7eb7b3ad26f3d3c0db32bd
EugeneGpil <gpilligrim@gmail.com>
2022-04-10 02:09:05 +0300

    1

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,26 +2,28 @@
   <div class="outer-wrapper">
     <div class="content-wrapper">
 
-      <div class="menu-container">
-        <div class="logo"></div>
-        <div class="menu-outer-item menu-outer-item1">
-          <div class="menu-inner-item">
-            <div class="menu-inner-item__logo"></div>
-            <div class="menu-inner-item__text">
-              <span class="nowrap">HELP THE</span>
-              CREATOR
+      <div class="menu-outer-container">
+        <div class="menu-container">
+          <div class="logo"></div>
+          <div class="menu-outer-item menu-outer-item1">
+            <div class="menu-inner-item">
+              <div class="menu-inner-item__logo"></div>
+              <div class="menu-inner-item__text">
+                <span class="nowrap">HELP THE</span>
+                CREATOR
+              </div>
             </div>
           </div>
-        </div>
-        <div class="menu-outer-item menu-outer-item2">
-          <div class="menu-inner-item">
-            <div class="menu-inner-item__logo"></div>
-            <div class="menu-inner-item__text">
+          <div class="menu-outer-item menu-outer-item2">
+            <div class="menu-inner-item">
+              <div class="menu-inner-item__logo"></div>
+              <div class="menu-inner-item__text">
               <span class="nowrap">
                 ABOUT
                 <span class="menu-inner-item__bigger-text">DRIP</span>
               </span>
-              CALCULATOR
+                CALCULATOR
+              </div>
             </div>
           </div>
         </div>
@@ -29,7 +31,7 @@
 
       <div class="content-outer-container">
         <div class="content-inner-container">
-          <div class="content-main-block">
+          <div class="content-main-block content-inner-container__block">
             <div class="content-main-block__headline content-main-headline">
               HYDRATION GAS FEE
             </div>
@@ -38,19 +40,31 @@
               <div class="main-value-underline"></div>
             </div>
           </div>
-          <div class="content-pair-block-container">
+          <div class="content-pair-block-container content-inner-container__block">
             <div class="content-pair-block">
-              <div class="content-pair-block__headline content-main-headline">DRIP PRICE</div>
-              <input class="pair-value-input" value="$ 0.0388">
-              <div class="pair-value-underline"></div>
+              <div class="
+                content-pair-block__headline
+                content-main-headline
+                content-pair-block-headline
+              ">
+                DRIP PRICE
+              </div>
+              <input class="main-value-input content-pair-block-input" value="$ 1293.33">
+              <div class="main-value-underline pair-value-underline"></div>
             </div>
             <div class="content-pair-block">
-              <div class="content-pair-block__headline content-main-headline">BNB PRICE</div>
-              <input class="pair-value-input" value="$ 400.3029">
-              <div class="pair-value-underline"></div>
+              <div class="
+                content-pair-block__headline
+                content-main-headline
+                content-pair-block-headline
+              ">
+                BNB PRICE
+              </div>
+              <input class="main-value-input content-pair-block-input" value="$ 1400.30">
+              <div class="main-value-underline pair-value-underline"></div>
             </div>
           </div>
-          <div class="content-main-block">
+          <div class="content-main-block content-inner-container__block">
             <div class="content-main-block__headline content-main-headline">
               DEPOSIT AMOUNT
             </div>
@@ -59,12 +73,16 @@
               <div class="main-value-underline"></div>
             </div>
           </div>
-          <div class="content-main-block">
-            <div class="content-main-block__headline content-main-headline">
-              THE AMOUNT TO HYDRATE PROPERLY
-            </div>
-            <div class="main-value-big-container">
-              <div class="main-value-big-value">0.777 DRIP</div>
+          <div class="content-main-block content-inner-container__block">
+            <div class="main-value-container main-value-result-container">
+              <div class="
+                main-value-result-container__headline
+                content-main-headline
+                main-value-result-headline
+              ">
+                THE AMOUNT TO HYDRATE PROPERLY
+              </div>
+              <div class="value-result">0.777 DRIP</div>
             </div>
           </div>
         </div>
